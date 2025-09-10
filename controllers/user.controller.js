29bf2edb60f3856b04ae54332e86b8689cc8a87b
yvashokkumarreddy <ro200259@rguktong.ac.in>
2025-09-11 02:30:24 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,7 +7,7 @@ import genertedRefreshToken from '../utils/generatedRefreshToken.js'
 import uploadImageClodinary from '../utils/uploadImageClodinary.js'
 import generatedOtp from '../utils/generatedOtp.js'
 import forgotPasswordTemplate from '../utils/forgotPasswordTemplate.js'
-import jwt from 'jsonwebtoken
+import jwt from 'jsonwebtoken'
 import CounterModel from '../models/counterModel.js'
 import bcryptjs from 'bcryptjs'
 
