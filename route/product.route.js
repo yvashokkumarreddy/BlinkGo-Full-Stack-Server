2b21e85b1801533bc4d0fb009816a34ed71092f7
yvashokkumarreddy <ashokreddyyeduru4@gmail.com>
2025-09-13 23:24:38 +0530

    =Changs related to products for admin features like update delete add

--- route/product.route.js
+++ route/product.route.js
@@ -5,17 +5,17 @@ import { admin } from '../middleware/Admin.js'
 
 const productRouter = Router()
 
-productRouter.post('/create',admin,createProductController)
+productRouter.post('/create',auth,admin,createProductController)
 productRouter.post('/get',getProductController)
 productRouter.post("/get-product-by-category",getProductByCategory)
 productRouter.post('/get-pruduct-by-category-and-subcategory',getProductByCategoryAndSubCategory)
 productRouter.post('/get-product-details',getProductDetails)
 
 //update product
-productRouter.put('/update-product-details',admin,updateProductDetails)
+productRouter.put('/update-product-details',auth,admin,updateProductDetails)
 
 //delete product
-productRouter.delete('/delete-product',admin,deleteProductDetails)
+productRouter.delete('/delete-product',auth,admin,deleteProductDetails)
 
 //search product 
 productRouter.post('/search-product',searchProduct)
