f26f238f0dab5de7d6c57abb073d581cd11d8b87
yvashokkumarreddy <ashokreddyyeduru4@gmail.com>
2025-09-18 22:45:49 +0530

    =updated orders realated to data import

--- route/product.route.js
+++ route/product.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js'
-import { createProductController, deleteProductDetails, getProductByCategory, getProductByCategoryAndSubCategory, getProductController, getProductDetails, searchProduct, updateProductDetails } from '../controllers/product.controller.js'
+import { createProductController, deleteProductDetails, downloadProductTemplate, getProductByCategory, getProductByCategoryAndSubCategory, getProductController, getProductDetails, searchProduct, updateProductDetails } from '../controllers/product.controller.js'
 import { admin } from '../middleware/Admin.js'
 
 const productRouter = Router()
@@ -10,14 +10,9 @@ productRouter.post('/get',getProductController)
 productRouter.post("/get-product-by-category",getProductByCategory)
 productRouter.post('/get-pruduct-by-category-and-subcategory',getProductByCategoryAndSubCategory)
 productRouter.post('/get-product-details',getProductDetails)
-
-//update product
 productRouter.put('/update-product-details',auth,admin,updateProductDetails)
-
-//delete product
 productRouter.delete('/delete-product',auth,admin,deleteProductDetails)
-
-//search product 
 productRouter.post('/search-product',searchProduct)
+productRouter.post('/download-template', downloadProductTemplate);
 
 export default productRouter
